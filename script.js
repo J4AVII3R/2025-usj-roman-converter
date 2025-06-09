91a8b2e743c8ccbde4a6296b43835d126c00c0a3
J4AVII3R <159425462+J4AVII3R@users.noreply.github.com>
2025-06-09 22:40:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function integerToRoman(number) {
+function integerToRomanFail(number) {
   if (typeof number !== 'number' || !Number.isInteger(number) || number < 1 || number > 3999) {
     throw new Error('Input must be an integer between 1 and 3999.');
   }
